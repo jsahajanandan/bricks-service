0d5be6c6bd4305fdbc6c560f9dcd50536d542005
jsahajanandan <jyothikasjdev@gmail.com>
2015-10-16 15:24:21 +0530

    Client friendly representation of enums in model serialization

--- app/models/development_plan.rb
+++ app/models/development_plan.rb
@@ -1,5 +1,14 @@
 class DevelopmentPlan < ActiveRecord::Base
   belongs_to :project
 
-  enum flat_type: { unknown: 0, BHK1: 1, BHK2: 2}
+  # There can be other flat types too otherwise store just numbers/ or types in separate db.
+  enum flat_type: { UNKNOWN: 0, BHK1: 1, BHK2: 2, BHK3: 3}
+
+  def serializable_hash(options={})
+    h = super(options)
+    if h['flat_type'].starts_with?('BHK') && h['flat_type'][3]
+      h['flat_type'] = h['flat_type'][3] + 'BHK'
+    end
+    h
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,5 +6,11 @@ class Project < ActiveRecord::Base
   accepts_nested_attributes_for :financial
 
   enum project_tag: {UNKNOWN: 0, HOT_INVESTMENT: 1, RISING: 2, POPULAR_LOCATION: 3, NEW: 4}
+
+  def serializable_hash(options={})
+    h = super(options)
+    h['project_tag'] = h['project_tag'].gsub('_', ' ')
+    h
+  end
 end
 
