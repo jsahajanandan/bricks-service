18d17ddecab19304d2b99e812c695a5e4f574a62
jsahajanandan <jyothikasjdev@gmail.com>
2015-10-17 09:49:04 +0530

    random fund data

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,8 +9,10 @@ class Project < ActiveRecord::Base
   enum project_tag: {UNKNOWN: 0, HOT_INVESTMENT: 1, RISING: 2, POPULAR_LOCATION: 3, NEW: 4}
   def serializable_hash(options={})
     h = super(options)
-    h['num_backers'] = (rand * 100).to_i
+    h['num_backers'] = (rand * 10000).to_i
     h['fund_raised'] = h['num_backers'] * financial.brick_value
+    h['fund_progress'] = (h['fund_raised']/financial.investment_sum_required * 100).to_i
+    h['fund_progress'] = 100 if h['fund_progress'] > 100
     h['project_tag'] = h['project_tag'].gsub('_', ' ')
     h
   end
